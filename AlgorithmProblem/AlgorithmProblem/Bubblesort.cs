0b3576fcc6a396f6a70a7f012d39f7b72ee95dc1
unknown <Saitharala@gmail.com>
2021-12-01 22:26:37 +0530

    [ Tharala SaiPrashanth] Added  Bubble Sort Problem  Feauture

--- AlgorithmProblem/AlgorithmProblem/Bubblesort.cs
+++ AlgorithmProblem/AlgorithmProblem/Bubblesort.cs
@@ -8,28 +8,28 @@ namespace AlgorithmProblem
 	{
 		public static void BubbleSortInArray(int[] arr)
 		{
-			int B = arr.Length;
-			for (int i = 0; i < B - 1; i++)
-			{
-				for (int j = 0; j < B - 1; j++)
-				{
-					if (arr[j] > arr[j + 1])
-					{
-						int temp = arr[j];
-						arr[j] = arr[j + 1];
-						arr[j + 1] = temp;
-					}
-				}
-			}
-		}
-		public static void PrintArray(int[] arr)
+		int B = arr.Length;
+		for (int i = 0; i < B - 1; i++)
 		{
-			Console.WriteLine("Using BubbleSort");
-			int B = arr.Length;
-			for (int i = 0; i < B; ++i)
-			{
-				Console.WriteLine(arr[i] + "");
-			}
+		for (int j = 0; j < B - 1; j++)
+		{
+		if (arr[j] > arr[j + 1])
+		{
+		int temp = arr[j];
+		arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+		 }
+	   }
+	}
+}
+	public static void PrintArray(int[] arr)
+	{
+		Console.WriteLine("Using BubbleSort");
+		int B = arr.Length;
+		for (int i = 0; i < B; ++i)
+		{
+	    	Console.WriteLine(arr[i] + "");
+		}
 			Console.WriteLine("\n");
 		}
 	}
